a8679a6aec7c9a717efef669e83d48f310f8bf3c
Ryan Graham <r.m.graham@gmail.com>
2016-05-03 16:55:26 -0700

    update/insert copyright notices

--- test/nodewho.test.js
+++ test/nodewho.test.js
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2013. All Rights Reserved.
+// Node module: nodewho
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 'use strict';
 
 var nodewho = require('../lib/nodewho.js');
